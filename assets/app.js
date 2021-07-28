1ce8bb899797e7fb95c67ff51221c91d442e84a1
Valdeth <alstontravis6@gmail.com>
2021-07-28 16:09:06 -0600

    added file structure and sound effects

---
+++ assets/app.js
@@ -0,0 +1,40 @@
+let time= 75;
+
+//create questions array
+let questions= [
+
+]
+
+
+//create HTML template (div, section, to anchor javaScript)
+
+
+//questionVariable = 0
+//function (questionVariable)
+function createQuestion() {
+
+    //declare variable to class question
+
+    //create element for question and append to questionVariable
+
+    //create buttons for choices and append to questionVariable
+
+}
+//populate questions to HTML through javaScript (going through array to make questions visable)
+//populate answers for users to choose from
+//function ends
+
+//eventListener goes here
+
+
+//convert the questions and answers into display next quesiton
+//assign a variable to current question on user click add one to current quesiton (questionVariable +++)
+//call function on line 7
+
+//saving to localStorage
+//get LocalStorage saved to an array, push new score to array, set to localStorage (use place holder score value to start off)
+
+//true false logic 
+
+//set interval (time) that will be used for scoring.
+//use global variable for time
