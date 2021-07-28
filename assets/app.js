10218fd8f40ea5a78484ac0fd1a4752a0983fd3a
Valdeth <alstontravis6@gmail.com>
2021-07-28 16:13:49 -0600

    added app logic

--- assets/app.js
+++ assets/app.js
@@ -1,40 +1,170 @@
-let time= 75;
+// variables to keep track of quiz state
+let currentQuestionIndex = 0;
+let time = questions.length * 15;
+let timerId;
 
-//create questions array
-let questions= [
+// variables to reference DOM elements
+const questionsEl = document.getElementById("questions");
+const timerEl = document.getElementById("time");
+const choicesEl = document.getElementById("choices");
+const submitBtn = document.getElementById("submit");
+const startBtn = document.getElementById("start");
+const initialsEl = document.getElementById("initials");
+const feedbackEl = document.getElementById("feedback");
 
-]
+// sound effects
+const sfxRight = new Audio("../sound effects/correct.wav");
+const sfxWrong = new Audio("../sound effects/incorrect.wav");
 
+function startQuiz() {
+  // hide start screen
+    const startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
 
-//create HTML template (div, section, to anchor javaScript)
+  // un-hide questions section
+    questionsEl.removeAttribute("class");
 
+  // start timer
+    timerId = setInterval(clockTick, 1000);
 
-//questionVariable = 0
-//function (questionVariable)
-function createQuestion() {
+  // show starting time
+    timerEl.textContent = time;
 
-    //declare variable to class question
-
-    //create element for question and append to questionVariable
-
-    //create buttons for choices and append to questionVariable
-
+    getQuestion();
 }
-//populate questions to HTML through javaScript (going through array to make questions visable)
-//populate answers for users to choose from
-//function ends
 
-//eventListener goes here
+function getQuestion() {
+  // get current question object from array
+    var currentQuestion = questions[currentQuestionIndex];
 
+  // update title with current question
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
 
-//convert the questions and answers into display next quesiton
-//assign a variable to current question on user click add one to current quesiton (questionVariable +++)
-//call function on line 7
+  // clear out any old question choices
+    choicesEl.innerHTML = "";
 
-//saving to localStorage
-//get LocalStorage saved to an array, push new score to array, set to localStorage (use place holder score value to start off)
+  // loop over choices
+    currentQuestion.choices.forEach(function(choice, i) {
+    // create new button for each choice
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
 
-//true false logic 
+    choiceNode.textContent = i + 1 + ". " + choice;
 
-//set interval (time) that will be used for scoring.
-//use global variable for time
+    // attach click event listener to each choice
+    choiceNode.onclick = questionClick;
+
+    // display on the page
+    choicesEl.appendChild(choiceNode);
+    });
+}
+
+function questionClick() {
+  // check if user guessed wrong
+    if (this.value !== questions[currentQuestionIndex].answer) {
+    // penalize time
+    time -= 15;
+
+    if (time < 0) {
+        time = 0;
+    }
+
+    // display new time on page
+    timerEl.textContent = time;
+
+    // play "wrong" sound effect
+    sfxWrong.play();
+
+    feedbackEl.textContent = "Wrong!";
+    } else {
+    // play "right" sound effect
+        sfxRight.play();
+
+        feedbackEl.textContent = "Correct!";
+    }
+
+  // flash right/wrong feedback on page for half a second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+    feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+  // move to next question
+    currentQuestionIndex++;
+
+  // check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+    quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+function quizEnd() {
+  // stop timer
+    clearInterval(timerId);
+
+  // show end screen
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
+
+  // show final score
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+
+  // hide questions section
+    questionsEl.setAttribute("class", "hide");
+}
+
+function clockTick() {
+  // update time
+    time--;
+    timerEl.textContent = time;
+
+  // check if user ran out of time
+    if (time <= 0) {
+    quizEnd();
+    }
+}
+
+function saveHighscore() {
+  // get value of input box
+    var initials = initialsEl.value.trim();
+
+  // make sure value wasn't empty
+    if (initials !== "") {
+    // get saved scores from localstorage, or if not any, set to empty array
+    var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // format new score object for current user
+    var newScore = {
+        score: time,
+        initials: initials
+    };
+
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // redirect to next page
+    window.location.href = "highscores.html";
+    }
+}
+
+function checkForEnter(event) {
+  // "13" represents the enter key
+    if (event.key === "Enter") {
+    saveHighscore();
+    }
+}
+
+// user clicks button to submit initials
+submitBtn.onclick = saveHighscore;
+
+// user clicks button to start quiz
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
